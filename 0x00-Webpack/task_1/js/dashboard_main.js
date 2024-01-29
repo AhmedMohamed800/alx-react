2c39e178eb632925816c5cc19748e66a0bbb8ca2
Ahmed Mohamed <ahmedmoh0107@gmail.com>
2024-01-29 18:42:25 +0200

    change files in task_1

--- 0x00-Webpack/task_1/js/dashboard_main.js
+++ 0x00-Webpack/task_1/js/dashboard_main.js
@@ -1,17 +1,15 @@
-import $ from "jquery";
+import $ from 'jquery';
 import _ from 'lodash';
-
 
 $('body').append('<p>Holberton Dashboard</p>');
 $('body').append('<p>Dashboard data for the students</p>');
 $('body').append('<button>Click here to get started</button>');
-$('body').append('<p id=\'count\'>Dashboard data for the students</p>');
-$('body').append('Copyright - Holberton School');
+$('body').append('<p id="count"></p>');
+$('body').append('<p>Copyright - Holberton School</p>');
 
 let count = 0;
 
-
-function updateCounter(){
+function updateCounter() {
     count++;
     $('#count').html(`${count} clicks on the button`);
 }
