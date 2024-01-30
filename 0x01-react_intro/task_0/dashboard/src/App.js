2cd3ae22653d3fa8c241307e3fc080db4e768d83
Ahmed Mohamed <ahmedmoh0107@gmail.com>
2024-01-30 19:41:34 +0200

    finish task 0

---
+++ 0x01-react_intro/task_0/dashboard/src/App.js
@@ -0,0 +1,20 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src="./holberton-logo.jpg" alt="Holberton Logo" className="holberton-logo" width="200px" height="200px" />
+        <h1>School Dashboard</h1>
+      </header>
+      <div className="App-body">
+        <p>Login to access the full dashboard</p>
+      </div>
+      <footer className="App-footer">
+        <p>Copyright 2022 - Holberton School</p>
+      </footer>
+    </div>
+  );
+}
+
+export default App;
