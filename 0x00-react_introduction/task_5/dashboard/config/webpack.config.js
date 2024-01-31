314b7faf20210c90e906f850c99cc9004675100d
Ahmed Mohamed <ahmedmoh0107@gmail.com>
2024-01-31 13:32:20 +0200

    Add webpack

--- 0x00-react_introduction/task_5/dashboard/config/webpack.config.js
+++ 0x00-react_introduction/task_5/dashboard/config/webpack.config.js
@@ -4,7 +4,7 @@ const path = require('path');
 const { CleanWebpackPlugin } = require('clean-webpack-plugin');
 
 module.exports = {
-    entry: './src/index.js',
+    entry: '../src/index.js',
     output: {
         path: path.resolve('dist'),
         filename: 'bundle.js',
