d4a49627afe7102b8c6dff915b8a824377dd3a65
Ahmed Mohamed <ahmedmoh0107@gmail.com>
2024-02-15 20:21:21 +0200

    aDD fix

--- 0x05-react_state/task_0/dashboard/src/App/App.js
+++ 0x05-react_state/task_0/dashboard/src/App/App.js
@@ -1,42 +1,45 @@
-import React from 'react';
-import Header from '../Header/Header';
-import Footer from '../Footer/Footer';
-import Login from '../Login/Login';
-import Notifications from '../Notifications/Notifications';
-import PropTypes from 'prop-types';
-import CourseList from '../CourseList/CourseList';
-import BodySectionWithMarginBottom from '../BodySection/BodySectionWithMarginBottom';
-import BodySection from '../BodySection/BodySection';
+import React from "react";
+import Header from "../Header/Header";
+import Footer from "../Footer/Footer";
+import Login from "../Login/Login";
+import CourseList from "../CourseList/CourseList";
+import Notifications from "../Notifications/Notifications";
+import BodySectionWithMarginBottom from "../BodySection/BodySectionWithMarginBottom";
+import BodySection from "../BodySection/BodySection";
 import { StyleSheet, css } from "aphrodite";
-
+import PropTypes from "prop-types";
+import { getLatestNotification } from "../utils/utils";
 
 class App extends React.Component {
   constructor(props) {
-    super(props)
-    this.state = {
-      displayDrawer: false
-    }
+    super(props);
+
+    this.state = { displayDrawer: false };
+
     this.handleKeyPress = this.handleKeyPress.bind(this);
     this.handleDisplayDrawer = this.handleDisplayDrawer.bind(this);
     this.handleHideDrawer = this.handleHideDrawer.bind(this);
   }
 
-  coursesList = [{ id: 1, name: 'ES6', credit: 60 },
-  { id: 2, name: 'Webpack', credit: 20 },
-  { id: 3, name: 'React', credit: 40 }]
+  listCourses = [
+    { id: 1, name: "ES6", credit: 60 },
+    { id: 2, name: "Webpack", credit: 20 },
+    { id: 3, name: "React", credit: 40 },
+  ];
 
-  notificationsList = [
-    { id: 1, value: 'New course available', type: 'default' },
-    { id: 2, value: 'New resume available', type: 'urgent' },
-  ]
+  listNotifications = [
+    { id: 1, type: "default", value: "New course available" },
+    { id: 2, type: "urgent", value: "New resume available" },
+    { id: 3, type: "urgent", html: getLatestNotification() },
+  ];
 
   handleKeyPress(e) {
     if (e.ctrlKey && e.key === "h") {
+      e.preventDefault();
       alert("Logging you out");
       this.props.logOut();
     }
   }
-
 
   handleDisplayDrawer() {
     this.setState({ displayDrawer: true });
@@ -46,7 +49,6 @@ class App extends React.Component {
     this.setState({ displayDrawer: false });
   }
 
-
   componentDidMount() {
     document.addEventListener("keydown", this.handleKeyPress);
   }
@@ -55,17 +57,28 @@ class App extends React.Component {
     document.removeEventListener("keydown", this.handleKeyPress);
   }
 
-
-
   render() {
     return (
-      <>
-        <Notifications displayDrawer={true} />
+      <React.Fragment>
         <div className={css(styles.App)}>
-          <Header />
-          <div className='App-body'>
-            {this.props.isLoggedIn ? <BodySectionWithMarginBottom type="Course list"><CourseList listCourses={this.coursesList} /></BodySectionWithMarginBottom> : <BodySectionWithMarginBottom type=" Log in to continue"><Login /></BodySectionWithMarginBottom>}
+          <div className="heading-section">
+            <Notifications
+              listNotifications={this.listNotifications}
+              displayDrawer={this.state.displayDrawer}
+              handleDisplayDrawer={this.handleDisplayDrawer}
+              handleHideDrawer={this.handleHideDrawer}
+            />
+            <Header />
           </div>
+          {this.props.isLoggedIn ? (
+            <BodySectionWithMarginBottom title="Course list">
+              <CourseList listCourses={this.listCourses} />
+            </BodySectionWithMarginBottom>
+          ) : (
+            <BodySectionWithMarginBottom title="Log in to continue">
+              <Login />
+            </BodySectionWithMarginBottom>
+          )}
           <BodySection title="News from the school">
             <p>
               Lorem ipsum dolor sit amet consectetur adipisicing elit. Perspiciatis at tempora odio, necessitatibus repudiandae reiciendis cum nemo sed asperiores ut molestiae eaque aliquam illo ipsa
@@ -74,7 +87,7 @@ class App extends React.Component {
           </BodySection>
           <Footer />
         </div>
-      </>
+      </React.Fragment>
     );
   }
 }
@@ -99,8 +112,5 @@ App.propTypes = {
   isLoggedIn: PropTypes.bool,
   logOut: PropTypes.func,
 };
-
-
-
 
 export default App;
