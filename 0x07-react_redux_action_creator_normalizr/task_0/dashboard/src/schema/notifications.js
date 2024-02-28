6958e2ac0fa17eb4eedb2918cd73e2121951530c
Ahmed Mohamed <ahmedmoh0107@gmail.com>
2024-02-28 20:38:06 +0200

    Add task_2

--- 0x07-react_redux_action_creator_normalizr/task_0/dashboard/src/schema/notifications.js
+++ 0x07-react_redux_action_creator_normalizr/task_0/dashboard/src/schema/notifications.js
@@ -1,6 +1,6 @@
 import * as notificationItem from "../../notifications.json";
 
 
-export default function getAllNotificationsByUser(userId) {
-    return userId.filter((user) => userId == user.author.id).map((user) => user.context)
+export function getAllNotificationsByUser(userId) {
+    return notificationItem.filter((user) => userId == user.author.id).map((user) => user.context)
 }
